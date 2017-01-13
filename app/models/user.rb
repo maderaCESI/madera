3cdc1d4a3ab853cbc443a905ef64fca74e70d14c
Romain <romain.locquet@viacesi.fr>
2017-01-13 15:22:35 +0000

    Modification css page home

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,7 +20,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable,
-         :recoverable, :rememberable, :trackable, :validatable, :lockable, :registerable
+         :recoverable, :rememberable, :trackable, :validatable, :lockable
          
          
   def self.find_first_by_auth_conditions(warden_conditions)
